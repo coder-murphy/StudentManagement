c86b2b04debf35e40269bf83d0f66d0c1eb1c757
oopsReach <oopsReach@foxmail.com>
2021-12-24 16:49:18 +0800

    编辑部分

--- StudentManagement/Controllers/HomeController.cs
+++ StudentManagement/Controllers/HomeController.cs
@@ -102,6 +102,7 @@ namespace StudentManagement.Controllers
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
+        [Route("edit")]
         public ViewResult Edit(int id)
         {
             Student student = studentRepository.GetStudent(id);
@@ -116,12 +117,57 @@ namespace StudentManagement.Controllers
             return View(studentEditViewModel);
         }
 
+        /// <summary>
+        /// 编辑
+        /// </summary>
+        /// <param name="studentEditViewModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Edit(StudentEditViewModel studentEditViewModel)
+        {
+            if(ModelState .IsValid)
+            {
+                Student student = studentRepository.GetStudent(studentEditViewModel.Id);
+                studentEditViewModel.Name = student.Name;
+                studentEditViewModel.Email = student.Email;
+                studentEditViewModel.Major = student.Major;
+                if(studentEditViewModel.Icon != null)
+                {
+                    if(studentEditViewModel.ExistingIconPath != null)
+                    {
+                        string path = Path.Combine(_webHostEnvironment.WebRootPath, "images", "avatars", studentEditViewModel.ExistingIconPath);
+                        System.IO.File.Delete(path);
+                    }
+                    student.Icon = ProcessUploadedFile(studentEditViewModel);
+                }
+                Student updated = studentRepository.SaveStudent(student);
+                return RedirectToAction("index");
+            }
+            return View(studentEditViewModel);
+        }
+
         [Route("allinfos")]
         public ViewResult Details()
         {
             var model = studentRepository.GetAllStudents();
             ViewData["PageTitle"] = "Student Details";
             return View(model);
+        }
+
+        private string ProcessUploadedFile(StudentInfoViewModel model)
+        {
+            string uniqueFileName = null;
+            if(model.Icon != null)
+            {
+                string upLoadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "avatars");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Icon.FileName;
+                string filePath = Path.Combine(upLoadFolder, uniqueFileName);
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Icon.CopyTo(fs);
+                }
+            }
+            return uniqueFileName;
         }
 
         private IWebHostEnvironment _webHostEnvironment;
